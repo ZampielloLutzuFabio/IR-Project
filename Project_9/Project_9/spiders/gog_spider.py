9f2f0a49a02c0ec4750e17a26a09f0d12cad6ee9
ZampielloLutzuFabio <fabiozampiello@gmail.com>
2022-11-22 19:12:55 +0100

    spider for gog and itchio

---
+++ Project_9/Project_9/spiders/gog_spider.py
@@ -0,0 +1,29 @@
+import scrapy
+
+class GogSpider(scrapy.Spider):
+    name = 'gog_spider'
+    start_urls = ['https://www.gog.com/en/games?tags=indie']
+    page = 1
+
+    def parse(self, response):
+        pages = int(response.css('button.small-pagination__item span::text').get())
+
+        for i in response.css('div.product-tile__info'):
+            title = i.css('.product-tile__title::attr(title)').get()
+            # author = i.css('.game_author a::text').get()
+            # genre = i.css('.game_genre::text').get()
+            # platform = i.css('.game_platform::text').get()
+            # href = i.css('.game_title a::attr(href)').get()
+
+            yield{
+                'title': title,
+                # 'author': author, 
+                # 'genre': genre,
+                # 'platform': platform,
+                # 'href': href
+            }
+
+
+        if self.page < pages:
+            self.page += 1
+            yield response.follow('https://www.gog.com/en/games?tags=indie&page=' + str(self.page), self.parse)    
---
+++ Project_9/Project_9/spiders/itchio_spider.py
@@ -0,0 +1,34 @@
+import scrapy
+
+class ItchioSpider(scrapy.Spider):
+    name = 'itchio_spider'
+    page = 1
+    start_urls = ['https://itch.io/games?page=' + str(page)]
+    def parse(self, response):
+        
+        for i in response.css('div.game_cell'):
+            title = i.css('.game_title a::text').get()
+            author = i.css('.game_author a::text').get()
+            genre = i.css('.game_genre::text').get()
+            platform = i.css('.game_platform > span::attr(title)').getall()
+            price = i.css('.price_value::text').get()
+            sale = i.css('.sale_tag::text').get()
+            href = i.css('.game_title a::attr(href)').get()
+
+            for a in range(len(platform)):
+                platform[a] = platform[a].replace("Download for ", "")
+
+            yield{
+                'title': title,
+                'author': author, 
+                'genre': genre,
+                'platform': platform,
+                'price' : price,
+                'sale' : sale,
+                'href': href
+            }
+
+        self.page += 1
+        if self.page < 20:
+            next_page = 'https://itch.io/games?page=' + str(self.page)
+            yield response.follow(next_page, self.parse)
